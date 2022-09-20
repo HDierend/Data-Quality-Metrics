509a51508912f3502e477093e17e6d213c9eaea0
hgoerner <hendrik.goerner@tu-dresden.de>
2022-09-20 13:51:19 +0200

    .

--- docs/source/general_quality/examples/correctness/correctness_dataframe.py
+++ docs/source/general_quality/examples/correctness/correctness_dataframe.py
@@ -6,7 +6,6 @@ import seaborn as sns
 # load dataset
 df = pd.read_csv(r"C:/Users/Datasets/beachwater.csv", delimiter=";")
 
-
 plt.figure(figsize=(16, 5))
 plt.subplot(1, 1, 1)
 sns.distplot(df["Water Temperature"])
