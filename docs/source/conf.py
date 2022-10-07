809b8948d109b51a69069a2b57227f6dfe19c845
hgoerner <hendrik.goerner@tu-dresden.de>
2022-10-07 13:52:33 +0200

    version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ html_static_path = ['_static']
 html_logo = "nfdi4ing_logo.png"
 html_theme_options = {
     'logo_only': True,
-    'display_version': False,
+    'display_version': True,
 }
 
 # -- Options for EPUB output
