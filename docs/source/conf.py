0c4f5483a45456df00fcba2a816e800f32a27783
hgoerner <hendrik.goerner@tu-dresden.de>
2022-11-15 09:52:50 +0100

    -

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ templates_path = ["_templates"]
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = 'logo_nfdi4ing_cmyk_hoch.svg.svg'
+html_logo = 'logo_nfdi4ing_cmyk_hoch.svg'
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
