7a9f488bea7256b19f449e2ddaf05d6ebd162169
hgoerner <hendrik.goerner@tu-dresden.de>
2023-10-13 09:47:48 +0200

    overwrite config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,41 +2,36 @@
 
 # -- Project information
 
-project = "Data Quality Metrics"
-copyright = "2022, NFDI4Ing"
-author = "TA Golo"
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
 
-release = "0.1"
-version = "0.1.0"
+release = '0.1'
+version = '0.1.0'
 
 # -- General configuration
 
 extensions = [
-    "sphinx.ext.duration",
-    "sphinx.ext.doctest",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.autosectionlabel",
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
-intersphinx_disabled_domains = ["std"]
+intersphinx_disabled_domains = ['std']
 
-templates_path = ["_templates"]
+templates_path = ['_templates']
 
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+
 html_logo = 'logo_nfdi4ing_cmyk_hoch.svg'
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
 
 # -- Options for EPUB output
-epub_show_urls = "footnote"
+epub_show_urls = 'footnote'
